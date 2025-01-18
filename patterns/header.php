04a723cb3a4eeabd97ae888bbb002ffc51816ab0
kielllll <eatarranza@gmail.com>
2025-01-18 21:52:14 +0800

    initial commit

---
+++ patterns/header.php
@@ -0,0 +1,14 @@
+<? php
+/**
+ * Title: Header
+ * Slug: green-olives/header
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description: Header with site title and navigation.
+ *
+ * @package GreenOlives
+ * @subpackage Green_Olives_Theme
+ * @since Green Olives 1.0
+ */
+
+?>
