8127201ed1d60ce44f0f3fa8ff774188e47a3d86
kielllll <eatarranza@gmail.com>
2025-01-20 08:33:14 +0800

    feat: alert pattern

---
+++ patterns/alert.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Alert
+ * Slug: green-olives/alert
+ * Categories: text
+ * Description: Alert with a message.
+ *
+ * @package GreenOlives
+ * @subpackage Green_Olives_Theme
+ * @since Green Olives 1.0
+ */
+
+?>
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"margin":{"left":"0","right":"0"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-right:0;margin-left:0">Sign up for free newsletters and get more Green Olives delivered to your inbox</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","justifyContent":"right","orientation":"horizontal","flexWrap":"nowrap"}} -->
+<div class="wp-block-buttons" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":"0px","width":"0px","style":"none"}},"fontSize":"small"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size"><a class="wp-block-button__link wp-element-button" style="border-style:none;border-width:0px;border-radius:0px">🗙</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
